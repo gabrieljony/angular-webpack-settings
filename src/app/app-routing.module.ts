23173be4d56526c7164fee180e6018d48fa678c7
Gabriel Jony Borges Silva <gabjony@gmail.com>
2019-05-28 00:06:08 -0300

    ng generate module app-routing --flat --module=app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BibliotecaUtilitariosComponent } from './biblioteca-utilitarios/biblioteca-utilitarios.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/biblioteca-utilitarios', pathMatch: 'full' },
+  { path: 'biblioteca-utilitarios', component: BibliotecaUtilitariosComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BibliotecaUtilitariosComponent } from './biblioteca-utilitarios/biblioteca-utilitarios.component';
+import { ButtonComponent } from './biblioteca-utilitarios/button/button.component';
+import { MenuComponent } from './biblioteca-utilitarios/menu/menu.component';
+import { WidgetPerfilComponent } from './biblioteca-utilitarios/widget-perfil/widget-perfil.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BibliotecaUtilitariosComponent,
+    ButtonComponent,
+    MenuComponent,
+    WidgetPerfilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
